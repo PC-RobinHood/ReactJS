2dd24797be2346924f48af482752a8be6a6ff45e
Justin Graham <support@pcrobinhood.com>
2019-08-14 15:24:15 -0500

    mapped routes to pages via index.js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { createBrowserHistory } from "history"
+import { Router, Route, Switch } from "react-router-dom"
+
+import "assets/scss/style.scss"
+
+import Home from "views/Home/Home.jsx"
+import Who from "views/Who/Who.jsx"
+import What from "views/What/What.jsx"
+import Events from "views/Events/Events.jsx"
+import Donate from "views/Donate/Donate.jsx"
+import Contact from "views/Contact/Contact.jsx"
+
+var hist = createBrowserHistory()
+
+ReactDOM.render(
+    <Router history={hist}>
+        <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/WhoWeAre" component={Who} />
+            <Route path="/WhatWeDo" component={What} />
+            <Route path="/Events" component={Events} />
+            <Route path="/Donate" component={Donate} />
+            <Route path="/Contact" component={Contact} />
+        </Switch>
+    </Router>,
+    document.getElementById("root")
+)
