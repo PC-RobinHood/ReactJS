6f72813bb082a51887cf9198216c9b8f7c6c65e1
Justin Graham <support@pcrobinhood.com>
2019-08-16 22:00:33 -0500

    added imports + constructor

---
+++ src/views/Home/Sections/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+import withStyles from "@material-ui/core/styles/withStyles";
+import ArrowUpwardRound from "@material-ui/icons/ArrowUpwardRounded";
+import {Element, Events, scroller, scroll} from "react-scroll";
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import Grid from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Parallex from "components/Parallex/Parallex.jsx"
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import componentStyle from "assets/jss/webpage/views/components/jsx";
+
+class Components extends React.Components {
+    constructor(props) {
+        super(prop);
+        this.scrollToTop = this.scrollToTop.bind(this);
+    }
+
+
+    
+}
