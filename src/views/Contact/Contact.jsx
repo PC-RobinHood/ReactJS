47c0d359181fd8bbaae1d6391d75f47ed84b79a4
Justin Graham <support@pcrobinhood.com>
2019-08-21 23:56:04 -0500

    contact page

---
+++ src/views/Contact/Contact.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import componentsStyle from "assets/jss/webapp/views/components.jsx";
+
+class Contact extends React.Components {
+    constructor(props) {
+        super(prop);
+        this.scrollToTop = this.scrollToTop.bind(this);
+    }
+
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
+
+    render() {
+    const { classes, ...rest } = this.props;
+    return (
+        <div>
+            <Header
+                brand=""
+                rightLinks={<HeaderLinks />}
+                fixed
+                color="transparent"
+                changeColorOnScroll={{
+                    height: 400,
+                    color: "white"
+                }}
+                {...rest}
+            />
+            <GridContainer className={classes.logo} >
+                <GridItem className={classes.logo} >
+                    <Link to="/">
+                        <img src={logo} className={classes.logo} ></img>
+                    </Link>
+                </GridItem>
+            </GridContainer>
+            <div className={classes.section}>
+                <div className={classes.container}>
+                    <GridContainer justify="center">
+                        <GridItem xs={12} sm={12} md={8}>
+                                <h4>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </h4>
+                        </GridItem>
+                    </GridContainer>
+                    <GridContainer justify="center">
+                        <GridItem xs={12} sm={12} md={8}>
+                                <h4>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </h4>
+                        </GridItem>
+                    </GridContainer>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+}
+
+export default withStyles(componentsStyle)(Contact);
